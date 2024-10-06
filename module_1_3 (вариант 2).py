e03d50d624581a29f234bcf266a23257a06f71e0
KovalskiySergey <kovalskiy.seregei@gmail.com>
2024-10-06 10:44:21 +0300

    Add files via upload

---
+++ module_1_3 (вариант 2).py
@@ -0,0 +1,15 @@
+from operator import truediv
+
+name = ('Sergey')
+print('Name:', name)
+age = 30
+print('Age:', age)
+age = age + 5
+print('New age:',age)
+name = 'is_student'
+print(name,':', bool(name))
+
+
+
+
+
